ec608ff7d2cb2bf686a9b0f17ad0f05e17c8422a
Abdel9327 <74155950+Abdel9327@users.noreply.github.com>
2022-08-29 02:32:46 +0200

    make it better

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,17 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ContactComponent } from './contact/contact.component';
+import { FormationComponent } from './formation/formation.component';
+import { HomeComponent } from './home/home.component';
+import { NousComponent } from './nous/nous.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '',   redirectTo: 'accueil', pathMatch: 'full' },
+  {path:'accueil',component:HomeComponent},
+  {path:'formation',component:FormationComponent},
+  {path:'mouvFormation',component:NousComponent},
+  {path:'contact',component:ContactComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,27 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-
+import { HomeComponent } from './home/home.component';
+import { ContactComponent } from './contact/contact.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {MatIconModule} from '@angular/material/icon';
+import { FormationComponent } from './formation/formation.component';
+import { NousComponent } from './nous/nous.component'
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    ContactComponent,
+    FormationComponent,
+    NousComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatIconModule,
+    FontAwesomeModule
   ],
   providers: [],
   bootstrap: [AppComponent]
